4b9078b05106f6f1d83b33968514778a9467ed2b
YangDong2002 <yangdong2002@stu.xjtu.edu.cn>
2020-10-22 16:40:02 +0800

    Small progress

--- kernel/bio.c
+++ kernel/bio.c
@@ -23,14 +23,16 @@
 #include "fs.h"
 #include "buf.h"
 
+#define HASHSIZE 13
+
 struct {
-  struct spinlock lock;
+  struct spinlock headlk[HASHSIZE];
   struct buf buf[NBUF];
 
   // Linked list of all buffers, through prev/next.
   // Sorted by how recently the buffer was used.
   // head.next is most recent, head.prev is least.
-  struct buf head;
+  struct buf heads[HASHSIZE];
 } bcache;
 
 void
@@ -38,19 +40,24 @@ binit(void)
 {
   struct buf *b;
 
-  initlock(&bcache.lock, "bcache");
 
   // Create linked list of buffers
-  bcache.head.prev = &bcache.head;
-  bcache.head.next = &bcache.head;
+  for(int i = 0; i < HASHSIZE; ++i) {
+  	initlock(&bcache.headlk[i], "bcache.bucket");
+		bcache.heads[i].prev = &bcache.heads[i];
+		bcache.heads[i].next = &bcache.heads[i];
+	}
   for(b = bcache.buf; b < bcache.buf+NBUF; b++){
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
+    b->next = bcache.heads[0].next;
+    b->prev = &bcache.heads[0];
     initsleeplock(&b->lock, "buffer");
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
+    bcache.heads[0].next->prev = b;
+    bcache.heads[0].next = b;
+		b->timestamp = ticks;
   }
 }
+
+int hash(uint dev, uint blockno) { return (1234 * dev + 5678 * blockno + 90) % HASHSIZE; }
 
 // Look through buffer cache for block on device dev.
 // If not found, allocate a buffer.
@@ -60,13 +67,15 @@ bget(uint dev, uint blockno)
 {
   struct buf *b;
 
-  acquire(&bcache.lock);
+	uint id = hash(dev, blockno);
+  acquire(&bcache.headlk[id]);
 
   // Is the block already cached?
-  for(b = bcache.head.next; b != &bcache.head; b = b->next){
+  for(b = bcache.heads[id].next; b != &bcache.heads[id]; b = b->next){
     if(b->dev == dev && b->blockno == blockno){
       b->refcnt++;
-      release(&bcache.lock);
+			b->timestamp = ticks;
+      release(&bcache.headlk[id]);
       acquiresleep(&b->lock);
       return b;
     }
@@ -74,6 +83,7 @@ bget(uint dev, uint blockno)
 
   // Not cached.
   // Recycle the least recently used (LRU) unused buffer.
+	
   for(b = bcache.head.prev; b != &bcache.head; b = b->prev){
     if(b->refcnt == 0) {
       b->dev = dev;
